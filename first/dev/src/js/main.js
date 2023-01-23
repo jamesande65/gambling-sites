62cdbf27efec927c8089a26fbcbfd4760e413d17
Nikk7 <n.sliyusar@gmail.com>
2023-01-23 19:26:43 +0200

    commit byNick

--- first/dev/src/js/main.js
+++ first/dev/src/js/main.js
@@ -1,21 +1,29 @@
-$(document).ready(function(){
-	//calls functions
-});
+// $(document).ready(function(){
+// 	//calls functions
+// });
 
-$(window).on('load', function () {
-	//calls functions
-});
+// $(window).on('load', function () {
+// 	//calls functions
+// });
 
-$(window).on('resize', function () {
-	//calls functions
-});
+// $(window).on('resize', function () {
+// 	//calls functions
+// });
 
-$(window).load(function() {
-	//calls functions
-});
+// $(window).load(function() {
+// 	//calls functions
+// });
 
-$(window).scroll(function() {
-	//calls functions
-});
+// $(window).scroll(function() {
+// 	//calls functions
+// });
 
 //functions
+// console.log('ok');
+const burgerBtn = document.querySelector('.burger');
+const headerMenu = document.querySelector('.header-menu')
+
+burgerBtn.addEventListener('click', () => {
+	burgerBtn.classList.toggle('burgerActive');
+	headerMenu.classList.toggle('headerMenuActive')
+})
