059b8f46e8d440b0e2d776cffc9815db66297c0d
Nikk7 <n.sliyusar@gmail.com>
2023-01-24 00:23:44 +0200

    commit by nick modal & acordeon

--- first/dev/src/js/main.js
+++ first/dev/src/js/main.js
@@ -21,9 +21,22 @@
 //functions
 // console.log('ok');
 const burgerBtn = document.querySelector('.burger');
-const headerMenu = document.querySelector('.header-menu')
+const headerMenu = document.querySelector('.header-menu');
+const burgerBlock = document.querySelector('.burgerBlock');
+const accBtn = document.querySelector('.accordeonBtn');
+const accContent = document.querySelector('.table-of-contents__list');
+const moreImg = document.querySelector('.moreImg')
+const body = document.querySelector('body');
+
 
 burgerBtn.addEventListener('click', () => {
 	burgerBtn.classList.toggle('burgerActive');
-	headerMenu.classList.toggle('headerMenuActive')
+	headerMenu.classList.toggle('headerMenuActive');
+	burgerBlock.classList.toggle('burgerDisabled');
+	body.classList.toggle('blockScroll');
+});
+accBtn.addEventListener('click', () => {
+	accContent.classList.toggle('table-of-contents-list-active');
+	moreImg.classList.toggle('moreImgActive');
+	
 })
