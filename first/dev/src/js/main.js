a83e0adbf969414ede9daaf41e8f385b6147ad4b
dmitry.nasyrkulov <dmitry.nasyrkulov@kps.com>
2023-05-06 21:34:33 +0300

    lang fix

--- first/dev/src/js/main.js
+++ first/dev/src/js/main.js
@@ -10,3 +10,19 @@ burgerBtn.addEventListener('click', () => {
 	burgerBlock.classList.toggle('burgerDisabled');
 	body.classList.toggle('blockScroll');
 });
+
+
+//lang
+const lang = document.querySelector('#lang');
+const langLis = document.querySelectorAll('#lang li');
+const langBtn = document.querySelector('.dropbtn');
+
+langBtn.addEventListener('click', () => {
+	lang.classList.toggle('show');
+});
+
+langLis.forEach(element => {
+	element.addEventListener('click', (e) => {
+		langBtn.innerHTML = e.target.closest('a').innerHTML;
+	})
+});
