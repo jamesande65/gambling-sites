c03cae8e726b13e126c949192b89079e36b7203b
dmitry.nasyrkulov <dmitry.nasyrkulov@kps.com>
2023-09-28 18:32:21 +0300

    plug2 added

---
+++ plug2/dev/src/js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	//calls functions
+	scrollToTop();
+	mobileMenu();
+
+	$(window).scroll(function() {
+		//calls functions
+		toggleScrollToTop();
+		handleHeaderOnScroll();
+	});
+});
+
+$(window).on('load', function () {
+	//calls functions
+});
+
+$(window).on('resize', function () {
+	//calls functions
+});
+
+// $(window).load(function() {
+// 	//calls functions
+// });
+
+//FUNCTIONS
+
+// Scroll to top button show/hide
+function toggleScrollToTop() {
+	if (window.scrollY >= 80) {
+		$('.js-scroll-top').addClass('active');
+	} else {
+		$('.js-scroll-top').removeClass('active');
+	}
+}
+
+function scrollToTop() {
+	let scrollToTopButton = $('.js-scroll-top');
+	scrollToTopButton.on('click', function() {
+		scrollToTopButton.hasClass('active') ?
+		$("html, body").animate({ scrollTop: 0 }, 500) :
+		null;
+	});
+};
+
+function handleHeaderOnScroll() {
+	if (window.scrollY >= 80) {
+		$('.js-header').addClass('active');
+	} else {
+		$('.js-header').removeClass('active');
+	}
+}
+
+function mobileMenu() {
+	let mobileMenuButton = $('.js-mobile-menu-toggle');
+	let mobileMenu = $('.js-mobile-menu');
+	mobileMenuButton.on('click', function() {
+		mobileMenu.toggleClass('active');
+	})
+
+	let mobileSubmenuButtons = $('.js-submenu-wrapper>a');
+	mobileSubmenuButtons.length && mobileSubmenuButtons.each((index, item) => {
+		$(item).on('click', e => {
+			e.preventDefault();
+			$(e.target).toggleClass('active');
+		});
+	});
+}
